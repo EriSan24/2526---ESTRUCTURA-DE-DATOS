fa12f868c819deeeaa367289668b5ab7d16b4b0b
Erick Sánchez <eb.sanchezt@uea.edu.ec>
2026-01-14 19:23:41 -0500

    tarea
    tarea

---
+++ semana05-1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TareaSemana5.Ejercicio1
+{
+    public class Asignatura
+    {
+        public string Nombre { get; set; }
+        public string Codigo { get; set; }
+        public int Creditos { get; set; }
+
+        public Asignatura() { }
+
+        public Asignatura(string nombre, string codigo, int creditos)
+        {
+            Nombre = nombre;
+            Codigo = codigo;
+            Creditos = creditos;
+        }
+
+        public override string ToString()
+        {
+            return $"{Codigo} - {Nombre} ({Creditos} créditos)";
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Asignatura asignatura1 = new Asignatura("Matemáticas", "MAT101", 4);
+            Asignatura asignatura2 = new Asignatura("Historia", "HIS202", 3);
+
+            Console.WriteLine(asignatura1);
+            Console.WriteLine(asignatura2);
+        }
+    }
+}
